0236ef2f9bd3b5c46103ce22590f169894bf53dd
samitu <samitufacebook@gmail.com>
2019-06-07 19:17:18 +0100

    Collision class created

---
+++ FirstTry/tipsyRogerProject/src/org/academiadecodigo/murlogs/tipsyRoger/CollisionDetector.java
@@ -0,0 +1,15 @@
+package org.academiadecodigo.murlogs.tipsyRoger;
+
+import org.academiadecodigo.simplegraphics.graphics.Rectangle;
+
+public class CollisionDetector {
+    private int X;
+    private int Y;
+    private int widthToX;
+    private int hightY;
+
+
+    public void isColliding(Rectangle rectangle){
+
+    }
+}

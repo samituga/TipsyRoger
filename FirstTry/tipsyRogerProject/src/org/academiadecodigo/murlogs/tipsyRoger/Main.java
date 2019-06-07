b2941fc220c02f774aa9d6fa6d4fac6134f95857
joaoafonso <joaoalbertoafonso@yahoo.com>
2019-06-07 23:44:22 +0100

    Added bottles, pictures not working

--- FirstTry/tipsyRogerProject/src/org/academiadecodigo/murlogs/tipsyRoger/Main.java
+++ FirstTry/tipsyRogerProject/src/org/academiadecodigo/murlogs/tipsyRoger/Main.java
@@ -1,13 +1,17 @@
 package org.academiadecodigo.murlogs.tipsyRoger;
 
 
+import org.academiadecodigo.murlogs.tipsyRoger.GameObjects.BottleTypes;
+import org.academiadecodigo.murlogs.tipsyRoger.GameObjects.Bottles;
 import org.academiadecodigo.murlogs.tipsyRoger.GameObjects.Characters.Playable.Player;
+import org.academiadecodigo.simplegraphics.pictures.Picture;
 
 public class Main {
     public static void main(String[] args) {
 
-        Game game = new Game(new Player());
+        Game game = new Game(new Player(), new Bottles(BottleTypes.BEER));
 
         game.init();
+
     }
 }

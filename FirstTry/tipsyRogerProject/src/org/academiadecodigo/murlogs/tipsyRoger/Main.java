2772708e00cf65a5e982c5576d725d20c6c47f2f
samitu <samitufacebook@gmail.com>
2019-06-07 23:51:08 +0100

    resolved merge conflits

--- FirstTry/tipsyRogerProject/src/org/academiadecodigo/murlogs/tipsyRoger/Main.java
+++ FirstTry/tipsyRogerProject/src/org/academiadecodigo/murlogs/tipsyRoger/Main.java
@@ -9,11 +9,8 @@ import org.academiadecodigo.simplegraphics.pictures.Picture;
 public class Main {
     public static void main(String[] args) {
 
-<<<<<<< HEAD
-        Game game = new Game(new Player(50));
-=======
-        Game game = new Game(new Player(), new Bottles(BottleTypes.BEER));
->>>>>>> 4bbd463ce602bf158535b6af2e63d89d7164f1f3
+
+        Game game = new Game(new Player(50), new Bottles(BottleTypes.BEER));
 
         game.init();
 

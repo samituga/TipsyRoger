7020bbd887b35b8cd6daf4e03cb108318fe8263e
samitu <samitufacebook@gmail.com>
2019-06-08 15:18:18 +0100

    nothing

--- FirstTry/tipsyRogerProject/src/org/academiadecodigo/murlogs/tipsyRoger/Map.java
+++ FirstTry/tipsyRogerProject/src/org/academiadecodigo/murlogs/tipsyRoger/Map.java
@@ -22,19 +22,16 @@ public class Map extends Colidable {
 
     @Override
     public int y() {
-        //System.out.println("map y is: " + rectangle.getY());
         return rectangle.getY();
     }
 
     @Override
     public int xToWidth() {
-        //System.out.println("map xToWidth is: " + (rectangle.getX() + rectangle.getWidth()));
         return (x() + rectangle.getWidth());
     }
 
     @Override
     public int yToHeight() {
-        //System.out.println("map yToHeight is: " + (rectangle.getY() + rectangle.getHeight()));
         return (y() + rectangle.getHeight());
     }
 

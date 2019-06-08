6fb100b00a0f804825d9432a3c1ec5ca6acf26bb
joaoafonso <joaoalbertoafonso@yahoo.com>
2019-06-08 13:20:45 +0100

    base enemys added, still need to implement move

--- FirstTry/tipsyRogerProject/src/org/academiadecodigo/murlogs/tipsyRoger/GameObjects/Characters/Enemy/EnemyTypes.java
+++ FirstTry/tipsyRogerProject/src/org/academiadecodigo/murlogs/tipsyRoger/GameObjects/Characters/Enemy/EnemyTypes.java
@@ -1,19 +1,32 @@
 package org.academiadecodigo.murlogs.tipsyRoger.GameObjects.Characters.Enemy;
 
+import org.academiadecodigo.simplegraphics.graphics.Rectangle;
+
 public enum EnemyTypes {
-    DRUNKEN("Drunken"),
-    BARMAN("Barman"),
-    CHINELO_MAMA("Chinelo Mama");
+    DRUNKEN,
+    BARMAN,
+    CHINELO_MAMA;
 
-    private String name;
+    Rectangle rectangle;
 
 
-    EnemyTypes(String name) {
-        this.name = name;
+    public static Enemy enemyFactory() {
+        int width = 20;
+        int height = 50;
+
+        int randomNumber = (int) (Math.random() * EnemyTypes.values().length);
+        EnemyTypes enemyTypes = EnemyTypes.values()[randomNumber];
+
+
+        switch (enemyTypes) {
+            case BARMAN:
+                return new Barman(new Rectangle(200, 200, width, height));
+            case DRUNKEN:
+                return new Drunken(new Rectangle(300, 300, width, height));
+            default:
+                return new Drunken(new Rectangle(300, 300, width, height));
+        }
     }
 
 
-    public String getName() {
-        return name;
-    }
 }

1b630ffecb66e973ae44aff932e6f7cfeeac53f9
samitu <samitufacebook@gmail.com>
2019-06-11 14:16:58 +0100

    Added map and rectangles to represent the hitboxes

--- SecondTry/src/org/academiadecodigo/murlogs/tipsyRoger/Game.java
+++ SecondTry/src/org/academiadecodigo/murlogs/tipsyRoger/Game.java
@@ -22,25 +22,56 @@ public class Game {
         this.roger = roger;
     }*/
 
+
     public void init() {
 
-        Field map = new Field(new Picture(0, 0, "LootHunter_Tileset.png"));
+        Field map = new Field(new Picture(0, 0, "roger02.jpg"));
         map.draw();
-        roger = new Player(new Picture(30, 30, "Roger_Smith.png"));
-        wallsLinkedList.add(new Walls(new Rectangle(300, 300, 100, 100)));
-        wallsLinkedList.add(new Walls(new Rectangle(32, 332, 193, 50)));
-        wallsLinkedList.add(new Walls(new Rectangle(132, 632, 193, 50)));
+        roger = new Player(new Picture(100, 120, "Roger_Smith.png"));
+        wallsLinkedList.add(new Walls(new Rectangle(240, 200, 108, 68)));
+        wallsLinkedList.add(new Walls(new Rectangle(277, 158, 60, 60)));
+        wallsLinkedList.add(new Walls(new Rectangle(178, 225, 60, 60)));
+        wallsLinkedList.add(new Walls(new Rectangle(272, 288, 60, 60)));
+        wallsLinkedList.add(new Walls(new Rectangle(356, 217, 60, 60)));
+        wallsLinkedList.add(new Walls(new Rectangle(732, 204, 119, 68)));
+        wallsLinkedList.add(new Walls(new Rectangle(682, 225, 55, 60)));
+        wallsLinkedList.add(new Walls(new Rectangle(771, 155, 60, 60)));
+        wallsLinkedList.add(new Walls(new Rectangle(857, 218, 60, 60)));
+        wallsLinkedList.add(new Walls(new Rectangle(767, 283, 60, 60)));
+        wallsLinkedList.add(new Walls(new Rectangle(489, 443, 110, 70)));
+        wallsLinkedList.add(new Walls(new Rectangle(432, 465, 60, 60)));
+        wallsLinkedList.add(new Walls(new Rectangle(520, 400, 60, 60)));
+        wallsLinkedList.add(new Walls(new Rectangle(520, 520, 60, 60)));
+        wallsLinkedList.add(new Walls(new Rectangle(600, 453, 60, 60)));
+        wallsLinkedList.add(new Walls(new Rectangle(1010, 163, 50, 380)));
+        wallsLinkedList.add(new Walls(new Rectangle(1010, 163, 170, 75)));
+        wallsLinkedList.add(new Walls(new Rectangle(1020, 443, 160, 100)));
+        wallsLinkedList.add(new Walls(new Rectangle(54, 1, 1145, 100)));
+        wallsLinkedList.add(new Walls(new Rectangle(0, 0, 50, 220)));
+        wallsLinkedList.add(new Walls(new Rectangle(0, 220, 40, 200)));
+        wallsLinkedList.add(new Walls(new Rectangle(0, 420, 20, 50)));
+        wallsLinkedList.add(new Walls(new Rectangle(0, 470, 15, 70)));
+        wallsLinkedList.add(new Walls(new Rectangle(0, 540, 10, 60)));
+        wallsLinkedList.add(new Walls(new Rectangle(0, 600, 7, 75)));
+        wallsLinkedList.add(new Walls(new Rectangle(1140, 100, 59, 140)));
+        wallsLinkedList.add(new Walls(new Rectangle(1160, 240, 39, 140)));
+        wallsLinkedList.add(new Walls(new Rectangle(1170, 380, 29, 140)));
+        wallsLinkedList.add(new Walls(new Rectangle(1175, 520, 24, 155)));
+        wallsLinkedList.add(new Walls(new Rectangle(0, 675, 1200, 0)));
+
+
+
         bottleLinkedList.add(BottleFactory.spawnBottle(200, 200));
         bottleLinkedList.add(BottleFactory.spawnBottle(230, 240));
         bottleLinkedList.add(BottleFactory.spawnBottle(300, 300));
-        enemiesLinkedList.add(new Barman(new Picture(350, 305, "Roger_Smith.png")));
-        enemiesLinkedList.add(new Barman(new Picture(400, 400, "Roger_Smith.png")));
-        enemiesLinkedList.add(new Barman(new Picture(450, 500, "Roger_Smith.png")));
-        enemiesLinkedList.add(new Barman(new Picture(200, 100, "Roger_Smith.png")));
-        enemiesLinkedList.add(new Barman(new Picture(300, 200, "Roger_Smith.png")));
-        enemiesLinkedList.add(new Drunken(new Picture(600, 500, "enemytester.png")));
-        enemiesLinkedList.add(new Drunken(new Picture(600, 200, "enemytester.png")));
-        npcLinkedList.add(new NPC(new Picture(300,400,"npctesting.png")));
+        //enemiesLinkedList.add(new Barman(new Picture(350, 305, "Roger_Smith.png")));
+        //enemiesLinkedList.add(new Barman(new Picture(400, 400, "Roger_Smith.png")));
+        //enemiesLinkedList.add(new Barman(new Picture(450, 500, "Roger_Smith.png")));
+        //enemiesLinkedList.add(new Barman(new Picture(200, 100, "Roger_Smith.png")));
+        //enemiesLinkedList.add(new Barman(new Picture(300, 200, "Roger_Smith.png")));
+        //enemiesLinkedList.add(new Drunken(new Picture(600, 500, "enemytester.png")));
+        //enemiesLinkedList.add(new Drunken(new Picture(600, 200, "enemytester.png")));
+        //npcLinkedList.add(new NPC(new Picture(300,400,"npctesting.png")));
     }
 
     public void start() {
@@ -68,6 +99,7 @@ public class Game {
             }
 
             roger.move();
+            System.out.println("x: " + roger.x() + "y: " + roger.y());
 
             for (NPC npc : npcLinkedList) {
                 roger.predictMovementsNPC(npc);
